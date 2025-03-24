710d8ab05981acc1fbb4afb9805c1e88db1ccedc
shisirthakuri <shahishisir087@gmail.com>
2025-03-25 00:13:54 +0530

    understanding useState hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,26 @@
-import React from "react"
-import Button from "./Button"
+import React, { useState } from "react"
+
 
 
 
 const Home = () => {
+
+//   const state = useState(0) //[0, function]
+// const state1 = state[0] //initial state
+// const state2 = state[1]
+const [count,setCount] = useState(0)
+
+function increment(){
+  setCount(count +1)
+}
+// const increment = ()=>{
+//   setCount(count +1)
+// }
+
   return (
     <>
-    <div>
-      <h1>this side from home</h1>
-      
-    <Button buttonText = "Home"/>
-    </div>
+    <h1>{count}</h1>
+    <button onClick={increment}>counter</button>
    </>
   )
 }
